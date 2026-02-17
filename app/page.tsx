959cc05d5eda1237a332dcda9fa17e0ae488023b
jacobadam <jacobanevitt@gmail.com>
2026-02-17 11:17:04 -0600

    add sections placeholder with id for main content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <section>
-      <div>Right Column</div>
-    </section>
+    <>
+      <section>
+        <div id="about">About</div>
+      </section>
+      <section>
+        <div id="tech-stack">Tech Stack</div>
+      </section>
+      <section>
+        <div id="experience">Experience</div>
+      </section>
+      <section>
+        <div id="projects">Projects</div>
+      </section>
+    </>
   );
 }
