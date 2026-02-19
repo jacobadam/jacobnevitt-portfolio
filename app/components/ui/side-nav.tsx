f849fb0ffc247a6783f686c316cbebe7608c36f3
jacobadam <jacobanevitt@gmail.com>
2026-02-19 15:22:30 -0600

    import nav type, accept props and map through navLinks

--- app/components/ui/side-nav.tsx
+++ app/components/ui/side-nav.tsx
@@ -3,7 +3,13 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function SideNav() {
+import { navLink } from "@/types/nav.types";
+
+interface navLinksProps {
+  navLinks: navLink[];
+}
+
+export default function SideNav({ navLinks }: navLinksProps) {
   const [activeSection, setActiveSection] = useState<string>(() => {
     if (typeof window === "undefined") return "";
     return window.location.hash.slice(1);
@@ -26,62 +32,22 @@ export default function SideNav() {
 
       <nav>
         <ul className="flex flex-col justify-center items-center max-w-3xl">
-          <li>
-            <Link
-              className="group relative pb-0.5"
-              href="#about"
-              onClick={() => handleClick("about")}
-            >
-              <span>About</span>
-              <span
-                className={`${
-                  activeSection === "about" ? "scale-x-100" : "scale-x-0"
-                } absolute bg-current bottom-0 left-0 w-full h-0.5 origin-left group-hover:scale-x-100 transition-transform duration-700`}
-              ></span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="group relative pb-0.5"
-              onClick={() => handleClick("tech-stack")}
-              href="#tech-stack"
-            >
-              Tech Stack
-              <span
-                className={`${
-                  activeSection === "tech-stack" ? "scale-x-100" : "scale-x-0"
-                } absolute bg-current bottom-0 left-0 w-full h-0.5 origin-left group-hover:scale-x-100 transition-transform duration-700`}
-              ></span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="group relative pb-0.5"
-              onClick={() => handleClick("experience")}
-              href="#experience"
-            >
-              Experience
-              <span
-                className={`${
-                  activeSection === "experience" ? "scale-x-100" : "scale-x-0"
-                } absolute bg-current bottom-0 left-0 w-full h-0.5 origin-left group-hover:scale-x-100 transition-transform duration-700`}
-              ></span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="group relative pb-0.5"
-              onClick={() => handleClick("projects")}
-              href="#projects"
-            >
-              Projects
-              <span
-                className={`${
-                  activeSection === "projects" ? "scale-x-100" : "scale-x-0"
-                } absolute bg-current bottom-0 left-0 w-full h-0.5 origin-left group-hover:scale-x-100 transition-transform duration-700`}
-              ></span>
-            </Link>
-          </li>
+          {navLinks.map((item, index) => (
+            <li key={index}>
+              <Link
+                className="group relative pb-0.5"
+                href={`#${item.href}`}
+                onClick={() => handleClick("about")}
+              >
+                <span>{item.title}</span>
+                <span
+                  className={`${
+                    activeSection === item.title ? "scale-x-100" : "scale-x-0"
+                  } absolute bg-current bottom-0 left-0 w-full h-0.5 origin-left group-hover:scale-x-100 transition-transform duration-700`}
+                ></span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
     </aside>
