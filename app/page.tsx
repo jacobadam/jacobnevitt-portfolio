d702a7ebad6fc62e93f625e7d5b26606917f88b7
jacobadam <jacobanevitt@gmail.com>
2026-03-05 12:49:16 -0600

    add placeholder sections to the main page, add ids and test useClientWrapper to make sure shared state is working correctly with side-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,206 @@
+"use client";
+// import { useActiveSection } from "@/app/hooks/useActiveSection";
+
+import { useClientWrapper } from "@/app/components/context/ClientWrapper";
+
 export default function Home() {
+  // const activeSection = useActiveSection();
+
+  const { isActive, setIsActive } = useClientWrapper();
+
+  console.log(isActive, "inside Page");
+
+  const trueOrFalse = () => {
+    return isActive ? setIsActive(false) : setIsActive(true);
+  };
+
   return (
     <>
-      <section>
-        <div id="about">About</div>
+      <section id="about">
+        <div>
+          <button onClick={trueOrFalse}>CLICK ME!!!!</button>
+          AboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+          AboutAboutAboutAboutAboutAboutAbout
+        </div>
       </section>
-      <section>
-        <div id="tech-stack">Tech Stack</div>
+      <section id="tech-stack">
+        <div>
+          Tech StackTech StackTech StackTech StackTech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech StackTech
+          StackTech StackTech StackTech Stack Tech Stack Tech Stack Tech Stack
+          Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech StackTech StackTech StackTech
+          StackTech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack
+          Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech StackTech StackTech StackTech StackTech Stack Tech Stack
+          Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech Stack Tech
+          Stack Tech Stack Tech Stack Tech Stack Tech Stack
+        </div>
       </section>
-      <section>
-        <div id="experience">Experience</div>
+      <section id="experience">
+        <div>
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+          ExperienceExperienceExperienceExperienceExperience Experience
+          Experience Experience Experience Experience Experience Experience
+          Experience Experience Experience Experience Experience Experience
+        </div>
       </section>
-      <section>
-        <div id="projects">Projects</div>
+      <section id="projects">
+        <div>
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+          ProjectsProjectsProjectsProjectsProjectsProjects Projects Projects
+          Projects Projects Projects Projects Projects Projects Projects
+          Projects Projects Projects Projects Projects Projects
+        </div>
       </section>
     </>
   );
