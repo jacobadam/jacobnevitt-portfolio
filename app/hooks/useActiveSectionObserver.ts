d50ae30aba0b249c6ab7be8a9a5144a8a9784a4c
jacobadam <jacobanevitt@gmail.com>
2026-03-10 12:15:52 -0600

    rename intersectionObserver hook and add type-safe section lookup table for IntersectionObserver

---
+++ app/hooks/useActiveSectionObserver.ts
@@ -0,0 +1,69 @@
+import { useEffect } from "react";
+
+type sectionValues = {
+  ratio: number;
+  visible: boolean;
+  id: sectionId;
+};
+
+const sectionIds = ["about", "techStack", "experience", "projects"] as const;
+
+type sectionId = (typeof sectionIds)[number];
+
+type sectionLookup = Record<sectionId, sectionValues>;
+
+export const useActiveSectionObserver = (
+  setActiveSection: (activeSection: string) => void,
+): void => {
+  useEffect(() => {
+    const sections = document.querySelectorAll("section[id]");
+
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px 0px -70% 0px",
+      threshold: [0, 0.25, 0.5, 0.75, 1],
+    };
+
+    const sectionLookupTable: sectionLookup = {
+      about: { ratio: 0, visible: false, id: "about" },
+      techStack: { ratio: 0, visible: false, id: "techStack" },
+      experience: { ratio: 0, visible: false, id: "experience" },
+      projects: { ratio: 0, visible: false, id: "projects" },
+    };
+
+    function isSectionId(value: string): value is sectionId {
+      return sectionIds.some((item) => item === value);
+    }
+
+    const observerCallback = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        const id = entry.target.id;
+
+        if (isSectionId(id)) {
+          sectionLookupTable[id] = {
+            ratio: entry.intersectionRatio,
+            id,
+            visible: entry.isIntersecting,
+          };
+        }
+      });
+
+      // if (visibleSections.length > 0) {
+      //   const mostVisibleSection = visibleSections.reduce((prev, current) =>
+      //     prev.intersectionRatio > current.intersectionRatio ? prev : current,
+      //   );
+      //   setActiveSection(mostVisibleSection.target.id);
+      // }
+    };
+
+    const observer = new IntersectionObserver(
+      observerCallback,
+      observerOptions,
+    );
+
+    sections.forEach((section) => {
+      observer.observe(section);
+    });
+    return () => observer.disconnect();
+  }, [setActiveSection]);
+};
