8c0ca9d70cdc8632dcfd86d491f8caeaa4b974fa
jacobadam <jacobanevitt@gmail.com>
2026-03-11 12:45:31 -0600

    update about section with styling and copy

--- app/components/sections/about-section.tsx
+++ app/components/sections/about-section.tsx
@@ -1,3 +1,18 @@
 export function AboutSection() {
-  return <section></section>;
+  return (
+    <div className="text-center p-8 bg-white/10 backdrop-blur-lg  border-white/20 shadow-lg">
+      <p className="mb-4">
+        After graduating from an award-winning coding bootcamp, I worked as a
+        frontend developer at SpareRoom, the UK&apos;s largest property rental
+        website, where I helped refine and improve core user experiences. I
+        later developed revenue-driving features at Article, one of
+        Canada&apos;s leading eCommerce companies.
+      </p>
+      <p>
+        Today I run Greenhouse Web Designs, where I build fast, modern, and
+        optimized websites for businesses that want clean design, strong
+        performance, and a solid technical foundation.
+      </p>
+    </div>
+  );
 }
