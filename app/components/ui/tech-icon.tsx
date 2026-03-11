071e4ab7a65bc2b5eee9246296a2cdbbce7e8045
jacobadam <jacobanevitt@gmail.com>
2026-03-11 14:54:31 -0600

    fix styling and layout on tech-icon component

--- app/components/ui/tech-icon.tsx
+++ app/components/ui/tech-icon.tsx
@@ -3,14 +3,13 @@ import Image from "next/image";
 type TechIconProps = {
   name: string;
   iconPath: string;
-  alt: string;
 };
 
-export function TechIcon({ name, iconPath, alt }: TechIconProps) {
+export function TechIcon({ name, iconPath }: TechIconProps) {
   return (
-    <li className="flex flex-row justify-center items-center gap-2">
-      <Image src={iconPath} alt={alt} width={30} height={30}></Image>
-      <span>{name}</span>
+    <li className="flex flex-row items-center gap-2">
+      <Image src={iconPath} alt="" width={30} height={30}></Image>
+      <span className="font-semibold">{name}</span>
     </li>
   );
 }
