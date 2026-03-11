cae18fdd9a0a982d8dc80fc077d7addc69eab1fc
jacobadam <jacobanevitt@gmail.com>
2026-03-11 14:55:52 -0600

    import TechIcon component and technologies data file and render out using a map

--- app/components/sections/tech-stack-section.tsx
+++ app/components/sections/tech-stack-section.tsx
@@ -1,3 +1,15 @@
+import { TechIcon } from "../ui/tech-icon";
+import { frontEndTechnologies } from "@/app/data/technologies";
+
 export function TechStackSection() {
-  return <section></section>;
+  return (
+    <>
+      <h2>Frontend</h2>
+      <ul className="flex flex-row flex-wrap gap-4 max-w-2xl">
+        {frontEndTechnologies.map((tech, key) => (
+          <TechIcon key={key} iconPath={tech.icon} name={tech.name} />
+        ))}
+      </ul>
+    </>
+  );
 }
