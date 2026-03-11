e12da94c342fd1f6d5441d9caedffe7ef5ecd42e
jacobadam <jacobanevitt@gmail.com>
2026-03-11 14:51:39 -0600

    add frontend technologies data for tech stack section

---
+++ app/data/icons-data.ts
@@ -0,0 +1,16 @@
+type Tech = {
+  name: string;
+  icon: string;
+};
+
+export const frontEndTechnologies: Tech[] = [
+  { name: "React", icon: "/react-logo.svg" },
+  { name: "TypeScript", icon: "/typescript-logo.svg" },
+  { name: "Next.js", icon: "/nextjs-logo.svg" },
+  { name: "JavaScript", icon: "/javascript-logo.svg" },
+  { name: "Vue.js", icon: "/vuejs-logo.svg" },
+  { name: "Tailwind CSS", icon: "/tailwindcss-logo.svg" },
+  { name: "Motion", icon: "/motion-logo.svg" },
+  { name: "GSAP", icon: "/gsap-logo.svg" },
+  { name: "Angular", icon: "/angular-logo.svg" },
+];
