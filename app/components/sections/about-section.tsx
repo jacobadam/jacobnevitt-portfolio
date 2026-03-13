e66915a6eeb3200fee02f7f9a56632c9e49eb36f
jacobadam <jacobanevitt@gmail.com>
2026-03-13 13:57:07 -0600

    add clickable links for companies

--- app/components/sections/about-section.tsx
+++ app/components/sections/about-section.tsx
@@ -1,17 +1,45 @@
 export function AboutSection() {
   return (
-    <div className="text-center p-8 bg-white/10 backdrop-blur-lg  border-white/20 shadow-lg">
+    <div className="">
       <p className="mb-4">
         After graduating from an award-winning coding bootcamp, I worked as a
-        frontend developer at SpareRoom, the UK&apos;s largest property rental
-        website, where I helped refine and improve core user experiences. I
-        later developed revenue-driving features at Article, one of
-        Canada&apos;s leading eCommerce companies.
+        frontend developer at{" "}
+        <span className="font-semibold text-secondary">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://www.spareroom.co.uk/"}
+          >
+            SpareRoom
+          </a>
+        </span>
+        , the UK&apos;s largest property rental website, where I helped refine
+        and improve core user experiences. I later developed revenue-driving
+        features at{" "}
+        <span className="font-semibold text-secondary">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://www.article.com/"}
+          >
+            Article
+          </a>
+        </span>
+        , one of Canada&apos;s leading eCommerce companies.
       </p>
       <p>
-        Today I run Greenhouse Web Designs, where I build fast, modern, and
-        optimized websites for businesses that want clean design, strong
-        performance, and a solid technical foundation.
+        Today I run{" "}
+        <span className="font-semibold text-secondary">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={"https://www.greenhousewebdesigns.com/"}
+          >
+            Greenhouse Web Designs
+          </a>
+        </span>
+        , where I build fast, modern, and optimized websites for businesses that
+        want clean design, strong performance, and a solid technical foundation.
       </p>
     </div>
   );
