6a3926f79c30eef0353d0e7c936218671e920b4a
jacobadam <jacobanevitt@gmail.com>
2026-03-16 12:00:12 -0600

    update elements and add styling to card

--- app/components/ui/experience-card.tsx
+++ app/components/ui/experience-card.tsx
@@ -7,11 +7,13 @@ export const ExperienceCard = () => {
       <ul>
         {ExperienceCardData.map((experience: ExperienceType) => (
           <li key={experience.id}>
-            <div className="shadow-lg bg-white/5 px-4 py-4 rounded-xl hover:bg-white/10 hover:backdrop-blur-md border border-primary hover:border-white/20">
-              <h1>{experience.company}</h1>
-              <h2>{experience.title}</h2>
-              <span>{experience.year}</span>
-              <p>{experience.description}</p>
+            <div className="shadow-lg bg-white/5 px-4 py-4 rounded-lg hover:bg-white/10 hover:backdrop-blur-md border border-primary hover:border-white/20 mb-2">
+              <h2 className="text-lg font-bold tracking-wide mb-2">
+                {experience.company}
+              </h2>
+              <h3 className="font-semibold mb-2">{experience.title}</h3>
+              <p className="font-extralight italic mb-2">{experience.year}</p>
+              <p className="font-medium">{experience.description}</p>
             </div>
           </li>
         ))}
