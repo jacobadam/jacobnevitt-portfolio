5e97f04dbeba9e6c71b12473b7b3dfb29233bf6c
jacobadam <jacobanevitt@gmail.com>
2026-03-16 15:58:28 -0600

    update breakpoints and text size for mobile

--- app/components/sections/tech-stack-section.tsx
+++ app/components/sections/tech-stack-section.tsx
@@ -9,7 +9,9 @@ export function TechStackSection() {
   return (
     <div className="rounded-xl hover:bg-white/10 hover:backdrop-blur-md border border-primary hover:border-white/20 shadow-lg md:px-4 md:py-4">
       <div className="flex flex-row gap-12 mb-10">
-        <h2 className="flex font-semibold text-xl text-white">Frontend</h2>
+        <h2 className="flex font-semibold text-sm md:text-base text-white">
+          Frontend
+        </h2>
         <ul className="flex flex-row flex-wrap gap-4">
           {frontEndTechnologies.map((tech, key) => (
             <TechIcon key={key} iconPath={tech.icon} name={tech.name} />
@@ -17,7 +19,9 @@ export function TechStackSection() {
         </ul>
       </div>
       <div className="flex flex-row gap-12 mb-10">
-        <h2 className="flex font-semibold text-xl text-white">Backend</h2>
+        <h2 className="flex font-semibold text-sm md:text-base text-white">
+          Backend
+        </h2>
         <ul className="flex flex-row flex-wrap gap-4">
           {backEndTechnologies.map((tech, key) => (
             <TechIcon key={key} iconPath={tech.icon} name={tech.name} />
@@ -25,7 +29,9 @@ export function TechStackSection() {
         </ul>
       </div>
       <div className="flex flex-row gap-12">
-        <h2 className="flex font-semibold text-xl text-white">Backend</h2>
+        <h2 className="flex font-semibold text-sm md:text-base text-white">
+          Backend
+        </h2>
         <ul className="flex flex-row flex-wrap gap-4">
           {toolsAndPlatforms.map((tech, key) => (
             <TechIcon key={key} iconPath={tech.icon} name={tech.name} />
