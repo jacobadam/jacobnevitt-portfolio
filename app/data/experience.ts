65f82d8d253897bdc82d6f1864a14dc9aa99b880
jacobadam <jacobanevitt@gmail.com>
2026-03-16 11:15:49 -0600

    add job experience data

---
+++ app/data/experience.ts
@@ -0,0 +1,34 @@
+type Experience = {
+  id: number;
+  company: string;
+  title: string;
+  year: string;
+  description: string;
+};
+
+export const ExperienceCardData: Experience[] = [
+  {
+    id: 1,
+    company: "Greenhouse Web Designs",
+    title: "Founding Developer",
+    year: "2024-Present",
+    description:
+      "As the founder of Greenhouse Web Designs, I've partnered with startups and small businesses to design and build high-performing, responsive websites using various front-end technologies such as React, Vue.js, Next.js, Angular and Tailwind CSS. My work includes developing custom components, integrating third-party APIs such as Google Maps, and optimizing SEO and accessibility to achieve top Lighthouse scores. I've also deployed front-end applications to platforms like Vercel and collaborated closely with clients to deliver tailored solutions under tight deadlines.",
+  },
+  {
+    id: 2,
+    company: "Article",
+    title: "Software Developer",
+    year: "2021-2024",
+    description:
+      "Working as a developer on the Storefront team, I built and maintained features for the company’s e-commerce web application, which serves millions of monthly users, leveraging Vue.js, React, and Next.js. I drove a 12% increase in conversions via a product page redesign and implemented a cross-sell component that generated $30K in sales within its first month. I also spearheaded a performance optimization initiative that improved the Lighthouse score by 25% and reduced load times by 8%, significantly enhancing user experience. Additionally, I optimized product discoverability by enhancing site search with Algolia.",
+  },
+  {
+    id: 3,
+    company: "Spareroom",
+    title: "Software Developer",
+    year: "2019-2020",
+    description:
+      "As a developer on the backend team, I contributed to the platform by developing RESTful APIs in Perl for managing listings, user accounts, and rental transactions. My responsibilities also included participating in the integration of secure payment processing within the payment system using Node.js, including working with the Stripe API. Additionally, I leveraged my full-stack capabilities to support internal teams by building the frontend of an admin tool with Angular for data access, utilizing MySQL to query and analyze customer data, and generating custom reports for marketing mailshots. I also enhanced team efficiency by creating automation scripts in Go for repetitive development tasks, including determining daily stand-up participants based on availability.",
+  },
+];
