7403fe0d5824c969157c3526eeb4a3f97195b883
jacobadam <jacobanevitt@gmail.com>
2026-03-19 12:59:21 -0600

    create function to map over description and match keywords to highlight and render out

--- app/components/ui/experience-card.tsx
+++ app/components/ui/experience-card.tsx
@@ -2,33 +2,37 @@ import { ExperienceCardData } from "@/app/data/experience";
 import { Experience } from "@/app/types/experience.types";
 
 export const ExperienceCard = () => {
+  function highlightText(text: string, keywords: string[]) {
+    const parts = text.split(new RegExp(`(${keywords.join("|")})`, "gi"));
+
+    return parts.map((part, i) =>
+      keywords.some((k) => k.toLowerCase() === part.toLowerCase()) ? (
+        <span key={i} className="text-white font-bold">
+          {part}
+        </span>
+      ) : (
+        part
+      ),
+    );
+  }
+
   return (
     <ul>
       {ExperienceCardData.map((experience: Experience) => (
         <li key={experience.id}>
-          <div className="flex flex-col md:p-4 rounded-2xl gap-1 scale-100 hover:scale-100 hover:opacity-100 opacity-80 hover:bg-white/10 hover:backdrop-blur-md border hover:border-white/20 mb-4 backdrop-blur-xl z-10 border-white/5 bg-white/5">
-            <span className="text-xs font-black uppercase tracking-widest text-secondary">
+          <div className="flex flex-col p-4 mb-4 rounded-2xl gap-1 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
+            <span className="text-xs font-semibold uppercase tracking-widest text-secondary">
               {experience.year}
             </span>
             <h2 className="text-2xl font-bold tracking-tighter text-white">
               {experience.company}
             </h2>
-            <h3 className="text-lg font-medium text-slate-300">
+            <h3 className="text-lg font-medium text-primary-foreground">
               {experience.title}
             </h3>
-            <p className="text-sm leading-relaxed text-slate-400 font-medium">
-              {experience.description}
+            <p className="text-base text-secondary-foreground">
+              {highlightText(experience.description, experience.keywords)}
             </p>
-            <ul className="pl-3">
-              {experience.highlights.map((highlight, index) => (
-                <li
-                  className="list-disc mt-3 text-sm text-white/80"
-                  key={index}
-                >
-                  {highlight}
-                </li>
-              ))}
-            </ul>
           </div>
         </li>
       ))}
