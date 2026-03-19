2d04fccc538eefcb0bcc4340abf4e05bd97c0fcc
jacobadam <jacobanevitt@gmail.com>
2026-03-19 12:21:05 -0600

    style card and import and render highlights list

--- app/components/ui/experience-card.tsx
+++ app/components/ui/experience-card.tsx
@@ -6,15 +6,29 @@ export const ExperienceCard = () => {
     <ul>
       {ExperienceCardData.map((experience: Experience) => (
         <li key={experience.id}>
-          <div className="md:p-4 rounded-lg hover:bg-white/10 hover:backdrop-blur-md border border-primary hover:border-white/20 mb-12 md:mb-0">
-            <h2 className="text-lg font-bold tracking-wide mb-2">
+          <div className="flex flex-col md:p-4 rounded-2xl gap-1 scale-100 hover:scale-100 hover:opacity-100 opacity-80 hover:bg-white/10 hover:backdrop-blur-md border hover:border-white/20 mb-4 backdrop-blur-xl z-10 border-white/5 bg-white/5">
+            <span className="text-xs font-black uppercase tracking-widest text-secondary">
+              {experience.year}
+            </span>
+            <h2 className="text-2xl font-bold tracking-tighter text-white">
               {experience.company}
             </h2>
-            <h3 className="font-semibold mb-2">{experience.title}</h3>
-            <p className="font-extralight italic mb-2 text-secondary-foreground">
-              {experience.year}
+            <h3 className="text-lg font-medium text-slate-300">
+              {experience.title}
+            </h3>
+            <p className="text-sm leading-relaxed text-slate-400 font-medium">
+              {experience.description}
             </p>
-            <p className="font-medium">{experience.description}</p>
+            <ul className="pl-3">
+              {experience.highlights.map((highlight, index) => (
+                <li
+                  className="list-disc mt-3 text-sm text-white/80"
+                  key={index}
+                >
+                  {highlight}
+                </li>
+              ))}
+            </ul>
           </div>
         </li>
       ))}
