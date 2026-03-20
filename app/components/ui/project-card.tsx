e519c3468e324881372504998a16d8136c47aa6a
jacobadam <jacobanevitt@gmail.com>
2026-03-20 14:43:36 -0600

    add group hover for project card with svg indicator

--- app/components/ui/project-card.tsx
+++ app/components/ui/project-card.tsx
@@ -13,10 +13,23 @@ export const ProjectCard = () => {
           target="_blank"
         >
           <li key={project.id}>
-            <div className="flex flex-col p-4 mb-4 rounded-lg gap-3 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
-              <h2 className="text-lg font-bold tracking-wide mb-2">
-                {project.name}
-              </h2>
+            <div className="group flex flex-col p-4 mb-4 rounded-lg gap-3 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
+              <div className="group relative flex w-fit items-center gap-1  transition-all group-hover:text-secondary">
+                <h2 className="text-xl font-bold tracking-wide mb-2">
+                  {project.name}
+                </h2>
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="h-4 w-4 transition-transform duration-500 group-hover:-translate-y-1 group-hover:translate-x-1"
+                >
+                  <path d="M7 17L17 7M17 7H7M17 7V17" />
+                </svg>
+              </div>
               <div className="flex flex-col-reverse lg:flex-row flex-start gap-4 items-start mb-2">
                 <div className="relative w-40 lg:w-1/4 h-28">
                   <Image
