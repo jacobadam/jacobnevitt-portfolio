0a68474934a9aff622f2e1dbf76349233c86dad4
jacobadam <jacobanevitt@gmail.com>
2026-03-20 13:26:54 -0600

    update nav order, header styling and section spacing

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@
 import { useActiveSectionObserver } from "@/app/hooks/use-active-section-observer";
 import { useClientWrapper } from "@/app/components/provider/client-wrapper";
 import { AboutSection } from "@/app/components/sections/about-section";
-import { TechStackSection } from "@/app/components/sections/tech-stack-section";
-import { ExperienceSection } from "@/app/components/sections/experience-section";
 import { ProjectsSection } from "@/app/components/sections/projects-section";
+import { ExperienceSection } from "@/app/components/sections/experience-section";
+import { TechStackSection } from "@/app/components/sections/tech-stack-section";
 
 export default function Home() {
   const { setActiveSection } = useClientWrapper();
@@ -14,27 +14,27 @@ export default function Home() {
 
   return (
     <>
-      <section id="about" className="md:px-4 py-4 md:pb-8 space-y-8">
-        <h1 className="sticky top-0 z-20 -mx-6 md:-mx-2 mb-4 w-screen px-6 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 font-semibold text-xl">
+      <section id="about" className="md:px-4 py-4 md:pb-8">
+        <h1 className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-3 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0 font-semibold text-xl">
           About
         </h1>
         <AboutSection />
       </section>
 
-      <section id="experience" className="md:px-4 py-4 md:py-8 space-y-8">
-        <h1 className="sticky top-0 z-20 -mx-6 md:-mx-2 mb-4 w-screen px-6 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 font-semibold text-xl">
-          Experience
-        </h1>
-        <ExperienceSection />
-      </section>
-      <section id="projects" className="md:px-4 py-4 md:py-8 space-y-8">
-        <h1 className="sticky top-0 z-20 -mx-6 md:-mx-2 mb-4 w-screen px-6 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 font-semibold text-xl">
+      <section id="projects" className="md:px-4 py-4 md:py-8">
+        <h1 className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0 font-semibold text-xl">
           Projects
         </h1>
         <ProjectsSection />
       </section>
-      <section id="tech-stack" className="md:px-4 py-4 md:py-8 space-y-8">
-        <h1 className="sticky top-0 z-20 -mx-6 md:-mx-2 mb-4 w-screen px-6 py-5 backdrop-blur lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 font-semibold text-xl">
+      <section id="experience" className="md:px-4 py-4 md:py-8">
+        <h1 className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0 font-semibold text-xl">
+          Experience
+        </h1>
+        <ExperienceSection />
+      </section>
+      <section id="tech-stack" className="md:px-4 py-4 md:py-8">
+        <h1 className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0 font-semibold text-xl">
           Tech Stack
         </h1>
         <TechStackSection />
