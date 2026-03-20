de2c7c805ed65787a77cb50d6a722b15c90fcb3e
jacobadam <jacobanevitt@gmail.com>
2026-03-20 13:30:09 -0600

    update card styling

--- app/components/ui/experience-card.tsx
+++ app/components/ui/experience-card.tsx
@@ -6,7 +6,7 @@ export const ExperienceCard = () => {
     <ul>
       {ExperienceCardData.map((experience: Experience) => (
         <li key={experience.id}>
-          <div className="flex flex-col p-4 mb-4 rounded-2xl gap-1 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
+          <div className="flex flex-col p-4 mb-4 rounded-lg gap-1 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
             <span className="text-xs font-semibold uppercase tracking-widest text-secondary">
               {experience.year}
             </span>
