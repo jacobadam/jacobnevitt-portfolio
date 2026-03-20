d81cb56333f2052028ee7bb682261b00273ac021
jacobadam <jacobanevitt@gmail.com>
2026-03-20 12:56:31 -0600

    update project-card style

--- app/components/ui/project-card.tsx
+++ app/components/ui/project-card.tsx
@@ -7,16 +7,21 @@ export const ProjectCard = () => {
     <ul>
       {ProjectData.map((project: Project) => (
         <li key={project.id}>
-          <div className="flex flex-col p-4 mb-4 rounded-2xl gap-1 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
+          <div className="flex flex-col p-4 mb-4 rounded-lg gap-3 scale-100 hover:scale-101 opacity-80 hover:opacity-100 bg-white/5 hover:bg-white/10 backdrop-blur-xl hover:backdrop-blur-md border border-white/5 hover:border-white/20">
             <h2 className="text-lg font-bold tracking-wide mb-2">
               {project.name}
             </h2>
-            <div className="flex flex-col-reverse lg:flex-row flex-start grow-2 gap-4 mb-2">
-              <div className="relative w-40 lg:w-1/4 h-20">
-                <Image src={project.imageUrl} alt="" fill />
+            <div className="flex flex-col-reverse lg:flex-row flex-start gap-4 items-start mb-2">
+              <div className="relative w-40 lg:w-1/4 h-28">
+                <Image
+                  src={project.imageUrl}
+                  alt=""
+                  fill
+                  className="object-cover rounded-md"
+                />
               </div>
               <div className="w-full lg:w-3/4">
-                <p className="font-medium">{project.description}</p>
+                <p className="text-base">{project.description}</p>
               </div>
             </div>
             <ul className="flex flex-row gap-2 flex-wrap">
