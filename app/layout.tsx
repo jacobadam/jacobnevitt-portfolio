ad13d8b88c322272a6c26a8ce4d54125326e5c17
jacobadam <jacobanevitt@gmail.com>
2026-03-21 12:42:45 -0600

    move Client Wrapper component

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,12 +42,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="grid grid-cols-1 lg:grid-cols-2 bg-primary text-primary-foreground px-4 lg:px-20 mx-auto">
-          <ClientWrapper>
+        <ClientWrapper>
+          <div className="grid grid-cols-1 lg:grid-cols-2 bg-primary text-primary-foreground px-4 lg:px-20 mx-auto">
             <SideNav navLinks={navLinks} />
             <main>{children}</main>
-          </ClientWrapper>
-        </div>
+          </div>
+        </ClientWrapper>
       </body>
     </html>
   );
