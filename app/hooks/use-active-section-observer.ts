1483abb4ceb06d9e3be42d79a9d3273c91a0cc83
jacobadam <jacobanevitt@gmail.com>
2026-03-21 17:01:45 -0600

    fix rootMargin

--- app/hooks/use-active-section-observer.ts
+++ app/hooks/use-active-section-observer.ts
@@ -20,7 +20,7 @@ export const useActiveSectionObserver = (
 
     const observerOptions = {
       root: null,
-      rootMargin: "-40% 0px -55% 0px",
+      rootMargin: "-30% 0px -30% 0px",
       threshold: [0, 0.25, 0.5, 0.75, 1],
     };
 
